0ee19514885cd58c547468f2e7c8c11ba157f0bf
“Kravid-Z” <kravidz64@gmail.com>
2021-02-12 01:08:37 +0100

    added => creatingMyBingo() window.onload

--- M1/W1/D9/index.js
+++ M1/W1/D9/index.js
@@ -1,6 +1,31 @@
-const numbersBingo = []
-for (let i = 0; i < 76; i++) {
-    let newnumber = [i + 1]
-    numbersBingo.push(newnumber);    
+window.onload = () => {
+    const numbersBingoTable = []
+
+    const creatingMyBingo = function ()  {
+      let myBingoTable = document.querySelector("#myBingoTable")
+      for (let i = 0; i < 76; i++) {
+      const childDiv = document.createElement("div")
+      childDiv.className = "bingoNumber"
+      const bingoNumber = document.createElement("h3")
+      bingoNumber.innerText = i +1
+      childDiv.appendChild(bingoNumber)
+      myBingoTable.appendChild(childDiv)
+      numbersBingoTable[i] = childDiv
+    }
+  }
+  creatingMyBingo()
 }
-console.log(numbersBingo)
+// const numbersBingoTable = []
+        
+// const creatingMyBingo = function ()  {
+//   let myBingoTable = document.querySelector("#myBingoTable")
+//   for (let i = 0; i < 76; i++) {
+//     const childDiv = document.createElement("div")
+//     childDiv.className = "bingoNumber"
+//     const bingoNumber = document.createElement("h3")
+//     bingoNumber.innerText = i +1
+//     childDiv.appendChild(bingoNumber)
+//     myBingoTable.appendChild(childDiv)
+//     numbersBingoTable[i] = childDiv
+//   }
+// }
