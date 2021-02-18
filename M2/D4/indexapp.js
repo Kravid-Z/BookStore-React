19b7481f96b0356e73c6c5b1a361084344a233fa
“Kravid-Z” <kravidz64@gmail.com>
2021-02-18 17:01:05 +0100

    first layer, first funct

---
+++ M2/D4/indexapp.js
@@ -0,0 +1,21 @@
+//Variables Globales
+
+const buttonlist = document.getElementsByClassName("participants-list")
+for (let i = 0; i < buttonlist.length; i++) {
+    buttonlist[i].addEventListener("click",creatinglists)
+}
+
+
+
+
+const creatinglists = ()=>{
+
+    switch (buttonlist[1]) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+}
