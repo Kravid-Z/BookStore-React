56936e0e5775e51bd14ef003904bb5d24248a188
“Kravid-Z” <kravidz64@gmail.com>
2021-03-17 12:22:47 +0100

    Data Userlist rendered

--- M3 JS ADVANCED/D7/app.js
+++ M3 JS ADVANCED/D7/app.js
@@ -1,37 +1,57 @@
 console.log("JScript connected");
 
+let tbody = document.getElementsByTagName("tbody");
+tbody[0] = "";
+let users;
 
 const fetchData = async () => {
-    try {
-      let response = await fetch(`https://jsonplaceholder.typicode.com/users`);
-      let users = await response.json();
-      console.log("Data Structure",users);
+  try {
+    let response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    users = await response.json();
+    console.log("Data Structure", users);
 
-      const input = document.querySelector(".form-control")
-      let queryU =""
-      input.addEventListener("change",()=>{
-          queryU = input.value
-          console.log(queryU);
-          return queryU  
-      })
-      
-        let names = []
-         users.forEach(user => {
-            names.push(user.name)
-        });
-      console.log(names);
+    const renderUser = (user) => {
+      return ` <tr>
+        <th scope="row">${user.id}</th>
+        <td>${user.name}</td>
+        <td>${user.phone}</td>
+        <td>${user.email}</td>
+      </tr>`;
+    };
+
+    const renderAll = (curr, user) => {
+      return curr + renderUser(user);
+    };
+    let allList = users.reduce(
+      (curr, user) => renderAll(curr, user),
+      ""
+    );
+    // console.log(allList);
+    tbody[0].innerHTML = allList;
+    // console.log(tbody[0]);
+
+    const input = document.querySelector(".form-control");
+    let queryU = "";
+    input.addEventListener("change", () => {
+      queryU = input.value;
+      console.log(queryU);
+      return queryU;
+    });
+
+    let names = [];
+    users.forEach((user) => {
+      names.push(user.name);
+    });
+    console.log(names);
 
     //   let search = names.filter((name)=>{
-    //       return queryU === name 
+    //       return queryU === name
     //   })
     //   console.log(search);
+  } catch (error) {}
+};
 
-
-    } catch (error) {}
-  };
-
-  fetchData();
-
+fetchData();
 
 //========================================================================================
 // const getData = () => {
@@ -46,7 +66,6 @@ const fetchData = async () => {
 //     })
 // }
 
-
 // window.onload = () =>{
-//     getData()    
+//     getData()
 //     }
