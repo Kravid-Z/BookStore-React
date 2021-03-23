0287940fb6236e53482a22c99b6909ea5345e2ae
“Kravid-Z” <kravidz64@gmail.com>
2021-03-23 15:52:56 +0100

    NavigationBarB_S component added

---
+++ M4-REACT/D2/bookstore-react/src/Components/NavigationBarB_S.jsx
@@ -0,0 +1,24 @@
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+
+const NavigationBarB_S = (props) => (
+  <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar.Brand href="#home">{props.title}</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">About</Nav.Link>
+      </Nav>
+      <Form inline>
+        <FormControl
+          type="text"
+          placeholder="Search your Book"
+          className="mr-sm-2"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Navbar>
+);
+
+export default NavigationBarB_S;
